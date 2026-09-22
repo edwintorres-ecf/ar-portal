9d1baaaa668e4cef66557ca803c4a570a6d32d6a
AR Portal Deploy <arportal@ecf.local>
2026-09-22 11:19:00 -0400

    Remove funds-signal investigation scripts

--- _zero.js
+++
@@ -1,5 +0,0 @@
-const payee = require('./payee');
-const idx = payee.getIndex();
-const keys = Object.keys(idx);
-console.log('index entries:', keys.length);
-console.log('sample:', JSON.stringify(idx[keys[0]]).slice(0, 400));
